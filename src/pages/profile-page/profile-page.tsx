e67530ed4d8ddd259aca87f2c421c4d7379bc538
victorhalimm <victorhalim18@gmail.com>
2024-06-14 14:45:34 +0700

    fix: snowy addition

--- src/pages/profile-page/profile-page.tsx
+++ src/pages/profile-page/profile-page.tsx
@@ -3,7 +3,7 @@ import MainTemplate from "@/templates/main-template"
 const ProfilePage = () => {
     return (
         <MainTemplate>
-            
+            <p>I Love to eat Snowy</p>
         </MainTemplate>
     )
 }
